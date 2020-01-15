1145baa291fe6438a2c9bb66226054f48bb9c5d2
Sander Willems <sander.willems@ugent.be>
2020-01-15 11:51:25 +0100

    initial working version

---
+++ ptm_parser.py
@@ -0,0 +1,8 @@
+import pyteomics
+from pyteomics import fasta
+
+fasta_file_name = "/home/sander/Documents/Proteomics/msqrob/HumanMycoCrap_2018_nr.fasta"
+progenesis_file_name = "/home/sander/Documents/Proteomics/msqrob/All_Peptides_MSqRob_Formatted.csv"
+out_file_name = "/home/sander/Documents/Proteomics/msqrob/All_Peptides_MSqRob_Formatted.csv"
+
+proteins =
